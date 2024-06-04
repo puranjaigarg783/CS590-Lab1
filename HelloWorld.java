9b3727ab85d0745494e4a5ce51163442f211226c
pierrearchambeau <psarchambeau@dons.usfca.edu>
2024-06-04 11:43:55 -0700

    Hello World Program now prints "Hello New Mars" Issue 1

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,8 +1,9 @@
-
-
+/**
+ * Prints Hello New Mars
+ */
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Hello New Mars");
 
     }
 }
